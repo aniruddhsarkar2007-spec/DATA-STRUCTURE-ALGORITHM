b8fe56b43f9bad92e689978513170b4e258aff9c
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-09-17 23:28:56 +0530

    DSA

---
+++ function.py
@@ -0,0 +1,28 @@
+# USER DEFINED:
+
+# function without return value (Non return function)
+
+# 1. Without parameter
+# 2. With parameter
+
+#-------------------------------------------------------------------------------
+
+# Without Parameter - this will only print not return / (READ ONLY FUNCTION)
+# We will never use and give input function in the without parameter function
+
+# Syntax:
+
+#  def functionName():
+#     statement/code
+
+def show():
+    print("Welcome")
+show()    
+
+
+def joy():
+    a=9
+    b=4
+    print(a+b)
+joy()    
+
