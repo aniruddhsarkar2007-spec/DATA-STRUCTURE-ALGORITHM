4a622811ef63ed2c3720716cde9d5bc44c00f278
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-09-18 16:57:15 +0530

    DSA

--- function.py
+++ function.py
@@ -15,14 +15,75 @@
 #  def functionName():
 #     statement/code
 
-def show():
-    print("Welcome")
-show()    
 
 
-def joy():
-    a=9
-    b=4
-    print(a+b)
-joy()    
+# def show():
+#     print("Welcome")
+# show()     
+
+
+# def joy():
+#     a=9
+#     b=4
+#     print(a+b)
+# joy()    
+
+
+
+# WE CAN MAKE CHANGES IN RETURN TYPE BUT CAN NOT MAKE ANY CHANGES IN NON RETURN TYPE
+
+# IN NO RETURN WE USE PRINT AND IN RETURN WE DONT HAVE TO PRINT IT PRINT DIRECT
+
+#------------FUNCTION WITHOUT PARAMETER AND NO RETURN TYPE--------------
+
+# IT IS NO RETURN BECAUSE WE DONT USED RETURN KEYWORD
+
+
+# def student():
+#     print("Hello")
+# def show():
+#     a=10000
+#     print("ur balance is=",a)   
+# student() 
+# show()   
+
+
+#------------FUNCTION WITH PARAMETER AND NO RETURN TYPE--------------
+
+# def divi(a,b):
+#     print(a//b)
+# divi(20,5)    
+
+
+# def divi(a,b):
+#     print(a//b)
+
+# k=int(input("Enter 1st:")) 
+# j=int(input("Enter 2nd:"))
+# divi(k,j)
+
+
+# def swp(a,b):
+#     a,b=b,a
+#     print("a=",a)
+#     print("b=",b)
+# k=int(input("1st input"))   
+# j=int(input("2nd input"))     
+# swp(k,j)
+
+# def swp(a,b):
+#     c=a
+#     a=b
+#     b=c
+#     print("a=",a)
+#     print("b=",b)
+# k=int(input("1st input"))   
+# j=int(input("2nd input"))     
+# swp(k,j)
+
+
+
+# 1. W.A.P TO DISPLAY THE SUM OF THREE DIGIT NUMBER USING FUNCTION IF THE NO IS 3 DIGIT PRINT SUM ELSE PRINT UNVALID 
+
+# 2. W.A.P TO PRINT TABLE OF ANY NUMBER
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+
+# def swp(a,b):
+#     a,b=b,a
+#     print("a=",a)
+#     print("b=",b)
+# k=int(input("1st input"))   
+# j=int(input("2nd input"))     
+# swp(k,j)
